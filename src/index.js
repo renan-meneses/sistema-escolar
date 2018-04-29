eaa64c5c98ac12f6619aee5b394248fe4d7d52aa
João Vitor Cavalcante <joaovitor.c.barbosa9888@gmail.com>
2018-04-29 18:43:44 -0300

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 const server = require('./config/server')
 
-server.listen(3000, () => console.log('server running at port 3000'))
+server.listen(3000, () => {
+  console.log('server running at port 3000')
+})
